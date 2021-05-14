08de8e9601fe00811819cf0280988a5199a3f2ff
menglijinghua <508304482@qq.com>
2021-05-14 15:24:50 +0800

    添加了根据分类Id查询商品的功能

--- service/service_customer/src/main/java/back/customer/controller/ProductInfoController.java
+++ service/service_customer/src/main/java/back/customer/controller/ProductInfoController.java
@@ -2,10 +2,10 @@ package back.customer.controller;
 
 
 import back.common_utils.R;
-import back.customer.entity.OrderDetail;
 import back.customer.entity.ProductInfo;
 import back.customer.service.ProductInfoService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,20 +13,53 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+/**
+ * <p>
+ * 前端控制器
+ * </p>
+ *
+ * @author plyn
+ * @since 2021-05-14
+ */
 @RestController
-@RequestMapping("/customer/search")
+@RequestMapping("/customer/product-info")
+@Api("Customer Search Controller")
 @CrossOrigin
-public class SearchController {
+public class ProductInfoController {
 
     @Autowired
     private ProductInfoService productInfoService;
 
-    //通过商品名称模糊查找相应商品
+
+    /**
+     * @param categoryId
+     * @return R
+     * @author 钟保明
+     */
+    @ApiOperation(value = "根据categoryid查询商品")
+    @GetMapping("/searchByCategoryId/{categoryId}")
+    public R searchByCategoryId(@ApiParam(name = "categoryid", value = "类别id")
+                                @PathVariable Integer categoryId) {
+        QueryWrapper<ProductInfo> wrapper = new QueryWrapper<>();
+        wrapper.eq("category_id", categoryId);
+        List<Object> productInfos = productInfoService.listObjs(wrapper);
+        if (productInfos.isEmpty()) {
+            return R.error();
+        } else {
+            return R.ok().data("productInfos", productInfos);
+        }
+    }
+
+
+    /**
+     * @param productName
+     * @return R
+     * @author 王智
+     */
     @ApiOperation(value = "通过商品名称模糊查找相应商品")
     @GetMapping("SearchByPartName/{productName}")
     public R SearchByPartName(@ApiParam(name = "productName", value = "商品关键字")
-                                    @PathVariable String productName) {
+                              @PathVariable String productName) {
         QueryWrapper<ProductInfo> queryWrapper = new QueryWrapper<>();
         queryWrapper.like("product_name", productName);
         List<Object> list = productInfoService.listObjs(queryWrapper);
@@ -36,7 +69,12 @@ public class SearchController {
             return R.ok().data("list", list);
     }
 
-    //在分类情况下，通过商品名称模糊查找相应商品
+    /**
+     * @param typeId
+     * @param productName
+     * @return R
+     * @author 王智
+     */
     @ApiOperation(value = "通过商品名称模糊查找相应商品")
     @GetMapping("SearchByTypeByPartName/{typeId}/{productName}")
     public R SearchByTypeByPartName(@ApiParam(name = "typeId", value = "分类Id", required = true)
