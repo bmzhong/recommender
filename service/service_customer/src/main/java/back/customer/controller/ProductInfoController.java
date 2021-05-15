77b10bc44f8cbe8b98402c7325946d4219022b8a
menglijinghua <508304482@qq.com>
2021-05-15 17:01:20 +0800

    将商品搜索功能移动到了卖家模块，并做了少量修改

--- service/service_customer/src/main/java/back/customer/controller/ProductInfoController.java
+++ service/service_customer/src/main/java/back/customer/controller/ProductInfoController.java
@@ -23,72 +23,8 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/customer/product-info")
-@Api("Customer Search Controller")
 @CrossOrigin
 public class ProductInfoController {
 
-    @Autowired
-    private ProductInfoService productInfoService;
-
-
-    /**
-     * @param categoryId
-     * @return R
-     * @author 钟保明
-     */
-    @ApiOperation(value = "根据categoryid查询商品")
-    @GetMapping("/searchByCategoryId/{categoryId}")
-    public R searchByCategoryId(@ApiParam(name = "categoryid", value = "类别id")
-                                @PathVariable Integer categoryId) {
-        QueryWrapper<ProductInfo> wrapper = new QueryWrapper<>();
-        wrapper.eq("category_id", categoryId);
-        List<Object> productInfos = productInfoService.listObjs(wrapper);
-        if (productInfos.isEmpty()) {
-            return R.error();
-        } else {
-            return R.ok().data("productInfos", productInfos);
-        }
-    }
-
-
-    /**
-     * @param productName
-     * @return R
-     * @author 王智
-     */
-    @ApiOperation(value = "通过商品名称模糊查找相应商品")
-    @GetMapping("SearchByPartName/{productName}")
-    public R SearchByPartName(@ApiParam(name = "productName", value = "商品关键字")
-                              @PathVariable String productName) {
-        QueryWrapper<ProductInfo> queryWrapper = new QueryWrapper<>();
-        queryWrapper.like("product_name", productName);
-        List<Object> list = productInfoService.listObjs(queryWrapper);
-        if (list.isEmpty())
-            return R.error().data("msg", "no data found");
-        else
-            return R.ok().data("list", list);
-    }
-
-    /**
-     * @param typeId
-     * @param productName
-     * @return R
-     * @author 王智
-     */
-    @ApiOperation(value = "通过商品名称模糊查找相应商品")
-    @GetMapping("SearchByTypeByPartName/{typeId}/{productName}")
-    public R SearchByTypeByPartName(@ApiParam(name = "typeId", value = "分类Id", required = true)
-                                    @PathVariable String typeId,
-                                    @ApiParam(name = "productName", value = "商品关键字")
-                                    @PathVariable String productName) {
-        QueryWrapper<ProductInfo> queryWrapper = new QueryWrapper<>();
-        queryWrapper.eq("category_id", typeId);
-        queryWrapper.like("product_name", productName);
-        List<Object> list = productInfoService.listObjs(queryWrapper);
-        if (list.isEmpty())
-            return R.error().data("msg", "no data found");
-        else
-            return R.ok().data("list", list);
-    }
 }
 
