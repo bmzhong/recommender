f8c5fabcefafbb2d9be93942c5c4446a8d5c3f17
xyc <1186386792@qq.com>
2021-05-15 17:16:07 +0800

    完成微信二维码支付功能

---
+++ service/service_customer/src/main/java/back/customer/service/PayLogService.java
@@ -0,0 +1,23 @@
+package back.customer.service;
+
+import back.customer.entity.PayLog;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.Map;
+
+/**
+ * <p>
+ * 支付日志表 服务类
+ * </p>
+ *
+ * @author xyc
+ * @since 2021-05-15
+ */
+public interface PayLogService extends IService<PayLog> {
+
+    Map createNative(String orderNo);
+
+    Map<String, String> queryPayStatus(String orderNo);
+
+    void updateOrderStatus(Map<String, String> map);
+}
