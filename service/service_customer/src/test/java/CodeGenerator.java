897ea3137f3c459c4aa9ecc97cb370b54d97ccbf
xyc <1186386792@qq.com>
2021-05-15 17:12:43 +0800

    完成微信二维码支付功能

--- service/service_customer/src/test/java/CodeGenerator.java
+++ service/service_customer/src/test/java/CodeGenerator.java
@@ -25,7 +25,7 @@ public class CodeGenerator {
         GlobalConfig gc = new GlobalConfig();
         String projectPath = System.getProperty("user.dir");
         gc.setOutputDir(projectPath + "/src/main/java");
-        gc.setAuthor("plyn");
+        gc.setAuthor("xyc");
         gc.setOpen(false); //生成后是否打开资源管理器
         gc.setFileOverride(false); //重新生成时文件是否覆盖
         gc.setServiceName("%sService");    //去掉Service接口的首字母I
@@ -57,7 +57,7 @@ public class CodeGenerator {
 
         // 5、策略配置
         StrategyConfig strategy = new StrategyConfig();
-        strategy.setInclude("customer_addr","customer_inf");
+        strategy.setInclude("pay_log");
         strategy.setNaming(NamingStrategy.underline_to_camel);//数据库表映射到实体的命名策略
         //strategy.setTablePrefix(pc.getModuleName() + "_"); //生成实体时去掉表前缀
 
