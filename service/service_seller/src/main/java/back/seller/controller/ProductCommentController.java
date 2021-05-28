95c7b3e66ddf17dc3c88d17a09ccbf3a82831316
vfdxvffd <2201986113@qq.com>
2021-05-28 10:46:11 +0800

    增加商品评论信息的添加

--- service/service_seller/src/main/java/back/seller/controller/ProductCommentController.java
+++ service/service_seller/src/main/java/back/seller/controller/ProductCommentController.java
@@ -1,6 +1,11 @@
 package back.seller.controller;
 
 
+import com.baomidou.mybatisplus.extension.api.R;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +21,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/seller/product-comment")
 public class ProductCommentController {
+
+    @Autowired
+    private ProductCommentService productCommentService;
+
+    /**
+     * @param productComment 传入的商品评论信息封装的对象
+     * @return 返回增加操作成功与否
+     * @author :张琦
+     */
+    @ApiOperation("增加商品评论")
+    @RequestMapping("/add/comment")
+    public void addComment (@ApiParam(value = "商品评论信息", required = true)
+                                @RequestBody ProductComment productComment) {
+        final boolean save = productCommentService.save(productComment);
+        if (save) {
+            return R.ok().data("commentId",productComment.getCommentId());
+        } else {
+            return R.error();
+        }
+    }
 
 }
 
