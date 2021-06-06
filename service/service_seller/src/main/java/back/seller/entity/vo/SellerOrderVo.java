efab2944e3a68bf1bc8a968aac39b9b4bb8c9a9e
menglijinghua <508304482@qq.com>
2021-06-06 17:19:51 +0800

    修正了发货状态

--- service/service_seller/src/main/java/back/seller/entity/vo/SellerOrderVo.java
+++ service/service_seller/src/main/java/back/seller/entity/vo/SellerOrderVo.java
@@ -1,2 +1,29 @@
-package back.seller.entity.vo;public class SellerOrderVo {
+package back.seller.entity.vo;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@Data
+public class SellerOrderVo {
+
+    @ApiModelProperty(value = "商品名称")
+    private String productName;
+
+    @ApiModelProperty(value = "购买商品数量")
+    private Integer productCnt;
+
+    @ApiModelProperty(value = "省")
+    private String province;
+
+    @ApiModelProperty(value = "市")
+    private String city;
+
+    @ApiModelProperty(value = "区")
+    private String district;
+
+    @ApiModelProperty(value = "详细地址")
+    private String address;
+
+    @ApiModelProperty(value = "邮编")
+    private String zip;
 }
